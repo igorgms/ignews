57d969a9c2595a1d2d11a8863f09960c798d689b
Igor Gomes <igorgmsf@gmail.com>
2022-02-19 23:17:33 -0300

    nextauth config

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedin = true;
-  return isUserLoggedin ? (
+  const { data: session } = useSession();
+  console.log(session);
+
+  return session ? (
     <button type='button' className={styles.signInButton}>
       <FaGithub color='#04d361' />
-      Igor Gomes Firmino
-      <FiX color='#737380' className={styles.closeIcon} />
+      {session?.user?.name}
+      <FiX
+        color='#737380'
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color='#eba417' />
       Sign in with Github
     </button>
